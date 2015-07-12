7af2503c2b912c03bb85a799dfee8af8c2ce82d4
jdchisholm <chisholm.joseph.d@gmail.com>
2015-07-12 19:22:56 -0300

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -1,1 +1,17 @@
-plot 1 ode
+plot1 <- function () {
+      
+      #Data is read in but skips to the desired date, then reads in the rows of data wanted for the assignment
+      data <- read.table("household_power_consumption.txt", sep=";", skip = grep("1/2/2007", readLines("household_power_consumption.txt")), nrows = 2880)
+
+      #Changed the data in the X column to numeric
+      data[,3] <- as.character(data[,3])
+      data[,3] <- as.numeric(data[,3])
+
+      #Open graphic device, create histogram, copy it to a png file, then close graphic device
+      quartz()
+      hist(data[,3], main="Global Active Power", xlab = "Global Active Power (kilowatts)", col = "Red")
+      
+      dev.copy(png, file = "plot1.png", width=480, height = 480)
+      dev.off()
+      
+}
