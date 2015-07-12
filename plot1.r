219316afd448dc3f3de54c06501080221a843b61
jdchisholm <chisholm.joseph.d@gmail.com>
2015-07-12 19:24:09 -0300

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -3,7 +3,7 @@ plot1 <- function () {
       #Data is read in but skips to the desired date, then reads in the rows of data wanted for the assignment
       data <- read.table("household_power_consumption.txt", sep=";", skip = grep("1/2/2007", readLines("household_power_consumption.txt")), nrows = 2880)
 
-      #Changed the data in the X column to numeric
+      #Changed the data in the Global_Active_Power column (column #3) to numeric
       data[,3] <- as.character(data[,3])
       data[,3] <- as.numeric(data[,3])
 
@@ -13,5 +13,4 @@ plot1 <- function () {
       
       dev.copy(png, file = "plot1.png", width=480, height = 480)
       dev.off()
-      
 }
